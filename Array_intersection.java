68e7ec9e57afb07d698f9d199259187fab0268c6
Pratham Y. Angre <144460880+praathamm@users.noreply.github.com>
2024-06-17 13:15:15 +0530

    Add files via upload

---
+++ Array_intersection.java
@@ -0,0 +1,19 @@
+package Interviews;
+
+public class Array_intersection {
+    public static void main(String args[])
+    {
+        int array1[]={1,2,3,4},array2[]={3,4,5,6};
+        for(int i=0;i<array1.length;i++)
+        {
+            for(int j=0;j<array2.length;j++)
+            {
+                if(array1[i]==array2[j])
+                {
+                    System.out.print(array1[i]+" ");
+                }
+            }
+        }
+
+    }
+}
